a05bc36761502a78f76b6da2caaa807fcd65fcd6
ruan-narici <ruannarici@hotmail.com>
2024-08-13 19:37:48 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/com/ruannarici/consultafipe/ConsultafipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ruannarici.consultafipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultafipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultafipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ruannarici/consultafipe/ConsultafipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ruannarici.consultafipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultafipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
